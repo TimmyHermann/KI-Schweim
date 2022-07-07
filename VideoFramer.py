139cdbf1c2539ca2591d073e66d4aa260243fe94
JohnLoeffel <philipp.wagner39@googlemail.com>
2022-07-07 10:24:43 +0200

    Update VideoFramer.py

--- VideoFramer.py
+++ VideoFramer.py
@@ -1,14 +1,38 @@
 import cv2
+import os 
 
 # Opens the Video file
-cap = cv2.VideoCapture('WIN_20220706_17_22_18_Pro.mp4')
-i = 0
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    cv2.imwrite('kang' + str(i) + '.jpg', frame)
-    i += 1
+cam = cv2.VideoCapture("C:\\Users\\phili\\Pictures\\Videoprojekte\Vergleich von generischen GraphenDatenbanken und RDF-Zentrierte GraphenDatenbanken.mp4")
 
-cap.release()
+try: 
+    #Einen Ordner mit dem Namen data erstellen
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+#Wenn nicht erstellt, dann error
+except OSError:
+    print ('Error: Creating direcotry of data')
+
+currentframe = 0
+
+while(True):
+
+    #reading from frame
+    ret,frame = cam.read()
+
+    if ret:
+        #if video still left continue creating images
+        name = './data/frame' + str(currentframe) + '.jpg'
+        print ('Creating...' + name)
+
+        #writing the extracted images
+        cv2.imwrite(name, frame)
+
+        #increasing counter so that will
+        #show how many frames are created
+        currentframe += 30
+    else:
+        break
+
+cam.release()
 cv2.destroyAllWindows()
