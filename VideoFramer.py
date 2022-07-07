0c46e15b90ff19e3bfcd39b30395d7135f027466
JohnLoeffel <philipp.wagner39@googlemail.com>
2022-07-07 11:29:39 +0200

    Update VideoFramer.py

--- VideoFramer.py
+++ VideoFramer.py
@@ -1,8 +1,9 @@
+from itertools import count
 import cv2
 import os 
 
 # Opens the Video file
-cam = cv2.VideoCapture("C:\\Users\\phili\\Pictures\\Videoprojekte\Vergleich von generischen GraphenDatenbanken und RDF-Zentrierte GraphenDatenbanken.mp4")
+cam = cv2.VideoCapture("C:\\Users\\phili\\Videos\\KI\\WIN_20220707_10_02_45_Pro.mp4")
 
 try: 
     #Einen Ordner mit dem Namen data erstellen
@@ -21,16 +22,17 @@ while(True):
     ret,frame = cam.read()
 
     if ret:
-        #if video still left continue creating images
-        name = './data/frame' + str(currentframe) + '.jpg'
-        print ('Creating...' + name)
-
-        #writing the extracted images
-        cv2.imwrite(name, frame)
-
+        if currentframe % 30 == 0:
+            # if video still left continue creating images
+            name = './data/frame' + str(currentframe) + '.jpg'
+            print ('Creating...' + name)
+            
+            #writing the extracted images
+            cv2.imwrite(name, frame)
+        
         #increasing counter so that will
         #show how many frames are created
-        currentframe += 30
+        currentframe += 1
     else:
         break
 
